69e3dcd0d0086e59f84d6c8abd943732473eb747
NihalSayyad <nihalsayyad912@gmail.com>
2020-08-23 20:59:36 +0530

    user module and configuration

--- api/user/models.py
+++ api/user/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default='Anonymous')
+    email = models.EmailField(max_length=254, unique=True)
+
+    #username = None
+
+    USERNAME_FEILD = 'email'
+    REQUIRED_FIEDLS = []
+
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+
+    session_token = models.CharField(max_length=10, default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
